585b3ed9e50ca1c66778be1c5593dabb056b6025
MARIELOS <MARIELOS@DESKTOP-455OQHK>
2018-06-19 10:36:15 -0600

    Calculadora

---
+++ src/Conversor/Binario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conversor;
+
+/**
+ *
+ * @author MARIELOS
+ */
+public class Binario implements Operator {
+
+    @Override
+    public String Convertir(int bin) {
+        String b = "";
+        while (bin != 0) {
+            int r = (int) (bin % 2);
+            b = r + b;
+            bin /= 2;
+        }
+        return b;
+    }
+}
---
+++ src/Factory/FactoryProducer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+import Aritmetica.FactoryAritmetica;
+import Conversor.FactoryOperator;
+
+/**
+ *
+ * @author MARIELOS
+ */
+public class FactoryProducer {
+
+    public static AbstractFactory getFactory(String type) {
+        switch (type) {
+            case "aritmetica":
+                return new FactoryAritmetica();
+            case "conversor":
+                return new FactoryOperator();
+        }
+
+        return null;
+    }
+}
